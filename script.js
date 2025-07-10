510aa318eed22105f1c073234202ebb24513a968
Davi Guedes <128005669+davigguedes@users.noreply.github.com>
2025-07-10 12:08:23 +0000

    API Gemini

--- script.js
+++ script.js
@@ -1,11 +1,91 @@
-const apiKeyInput = document.getElementById('apiKey');
-const gameSelect = document.getElementById('gameSelect');
-const questionInput = document.getElementById('questionInput');
-const askButton = document.getElementById('askButton');
-const aiResponse = document.getElementById('aiResponse');
-const form = document.getElementById('form');
+const apiKeyInput = document.getElementById('apiKey')
+const gameSelect = document.getElementById('gameSelect')
+const questionInput = document.getElementById('questionInput')
+const askButton = document.getElementById('askButton')
+const aiResponse = document.getElementById('aiResponse')
+const form = document.getElementById('form')
 
-const enviarFormulario = (event) => {
+const markdownToHTML = (text) => {
+    const converter = new showdown.Converter()
+    return converter.makeHtml(text)
+}
+
+// AIzaSyAXAW5YvL3pMiNpqVayK7rb61c7DHMcqxA
+const perguntarIA = async (question, game, apiKey) => {
+    const model = "gemini-2.5-flash"
+    const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
+    const pergunta = `
+        ## Especialidade
+        Você é um especialista assistente de meta para o jogo ${game}
+        
+        ## Tarefa
+        Você deve responder as perguntas do usuário com bbase no seu conhecimento do jogo, estratégias, build e dicas
+
+        ## Regras
+        - Se você não sabe a resposta, responda com 'Não sei' e não tente inventar uma resposta.
+        - Se a pergunta não está relacionada com o o jogo, responda com 'Essa pergunta não está relacionada ao jogo'.
+        - Considere a data atual ${new Date().toLocaleDateString()}
+        - Faça pesquisas atualizadas sobre o patch atual, baseado na data atual, para dar uma resposta coerente.
+        - Nunca responda itens qque você não tenha certeza de que existe no patch atual.
+
+        ## Resposta
+        
+
+        ## Exemplo de resposta
+    `
+
+    const contents = [{
+        role: "user",
+        parts: [{
+            text: pergunta
+        }]
+    }]
+
+    const tools = [{
+        google_search: {}
+    }]
+
+    // chamada API
+    const response = await fetch(geminiURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            contents,
+            tools
+        })
+    })
+
+    const data = await response.json()
+    return data.candidates[0].content.parts[0].text
+}
+
+const enviarFormulario = async (event) => {
     event.preventDefault()
+    const apiKey = apiKeyInput.value
+    const game = gameSelect.value
+    const question = questionInput.value
+
+    if(apiKey == '' || game == '' || question == '') {
+        alert('Por favor, preencha todos os campos')
+        return
+    }
+
+    askButton.disabled = true;
+    askButton.textContent = 'Perguntando...'
+    askButton.classList.add('loading')
+
+    try {
+        const text = await perguntarIA(question, game, apiKey)
+        aiResponse.querySelector('.response-content').innerHTML = markdownToHTML(text)
+
+    } catch(error) {
+        console.log('Erro: ', error)
+    } finally {
+        askButton.disabled = false
+        askButton.textContent = "Perguntar"
+        askButton.classList.remove('loading')
+    }
 }
 form.addEventListener('submit', enviarFormulario)
