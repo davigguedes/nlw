5fe1f725959278167a336036d6fdde20a3e436f9
Davi Guedes <128005669+davigguedes@users.noreply.github.com>
2025-07-10 17:58:03 +0000

    melhorar caixa da resposta da IA

--- script.js
+++ script.js
@@ -29,9 +29,16 @@ const perguntarIA = async (question, game, apiKey) => {
         - Nunca responda itens qque você não tenha certeza de que existe no patch atual.
 
         ## Resposta
-        
+        - Economize na resposta, seja direto e responda no máximo 500 caracteres.
+        - Responda em markdown
+        - Não precisa fazer nenhuma saudação ou despedida, apenas responda o que o usuário está querendo.
 
         ## Exemplo de resposta
+        pergunta do usuário: Melhor build rengar jungle
+        resposta: A build mais atual é \n\n **Itens:**\n\n coloque os itens aqui.\n\n**Runas:**\n\nexemplo de runas\n\n
+
+        ---
+        Aqui está a pergunta do usuário: ${question}
     `
 
     const contents = [{
@@ -79,7 +86,7 @@ const enviarFormulario = async (event) => {
     try {
         const text = await perguntarIA(question, game, apiKey)
         aiResponse.querySelector('.response-content').innerHTML = markdownToHTML(text)
-
+        aiResponse.classList.remove('hidden')
     } catch(error) {
         console.log('Erro: ', error)
     } finally {
